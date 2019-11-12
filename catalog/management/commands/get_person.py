1cd210ae36ce1dbd1064c350fdf83fd8129fb4e0
Ilya <kochinashviliilya@gmail.com>
2019-11-12 22:22:16 +0200

    sort done

--- catalog/management/commands/get_person.py
+++ catalog/management/commands/get_person.py
@@ -1,1 +1,43 @@
+from django.core.management.base import BaseCommand
 
+from openpyxl import load_workbook
+from catalog.models import Person
+
+
+def get_data():
+    person = {}
+    wb = load_workbook('./Persons list.xlsx')
+    sheet = wb.get_sheet_by_name('Sheet1')
+    data = sheet.values
+    n_data = []
+    for i in data:
+        n_data.append(i)
+    n_data = n_data[1:]
+
+    for i in n_data:
+        name = i[0]
+        points = i[1]
+        if i[2] == None:
+            photo = 'None_photo.jpg'
+        else:
+            photo = i[2]
+        facebook = i[3]
+        person = {
+            'name': name,
+            'points': points,
+            'photo': photo,
+            'facebook': facebook,
+        }
+        try:
+            Person.objects.create(**person)
+            print(person)
+        except Exception as e:
+            print(type(e), e)
+            continue
+
+
+class Command(BaseCommand):
+    help = 'Get data from excel file'
+
+    def handle(self, *args, **options):
+        get_data()
--- catalog/models.py
+++ catalog/models.py
@@ -7,8 +7,10 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=255)
     points = models.IntegerField()
-    photo = models.ImageField(upload_to="media")
-    facebook = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to="media", null=True)
+    facebook = models.CharField(max_length=255, unique=True)
+
+    objects = models.Manager()
 
     def __str__(self):
         return self.name
