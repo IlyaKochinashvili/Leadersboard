ee8eb6dc6b23d4b944103a6e7624816c71a3ddd0
Ilya <kochinashviliilya@gmail.com>
2019-11-16 19:49:55 +0200

    sort done

--- catalog/management/commands/get_person.py
+++ catalog/management/commands/get_person.py
@@ -1,11 +1,22 @@
-from django.core.management.base import BaseCommand
+import os
 
+from django.core.exceptions import ValidationError
+from django.core.management.base import BaseCommand
+from threading import Thread
 from openpyxl import load_workbook
+
+from catalog import models
 from catalog.models import Person
+
+time_stamp = os.path.getmtime('./Persons list.xlsx')
+
+
+def update():
+    # Person.objects.filter().delete()
+    get_data()
 
 
 def get_data():
-    person = {}
     wb = load_workbook('./Persons list.xlsx')
     sheet = wb.get_sheet_by_name('Sheet1')
     data = sheet.values
@@ -17,7 +28,7 @@ def get_data():
     for i in n_data:
         name = i[0]
         points = i[1]
-        if i[2] == None:
+        if i[2] is None:
             photo = 'None_photo.jpg'
         else:
             photo = i[2]
@@ -33,11 +44,11 @@ def get_data():
             print(person)
         except Exception as e:
             print(type(e), e)
-            continue
+        continue
 
 
 class Command(BaseCommand):
     help = 'Get data from excel file'
 
     def handle(self, *args, **options):
-        get_data()
+        update()
--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
-
+from catalog import models
 from catalog.models import Person
 
 
 # Create your views here.
-
 def index_view(request):
     persons = Person.objects.order_by('-points')[:10]
     context = {'persons': persons, }
     return render(request, 'index.html', context)
-
 
