e9b8c8fac0af8e704c58c554b2658ee6065e7fea
Ilya <kochinashviliilya@gmail.com>
2019-12-17 16:34:58 +0200

    Update index.html

--- leaderboard/settings_drod.py
+++ leaderboard/settings_drod.py
@@ -5,8 +5,8 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db1',
-        'USER': 'leadersboard',
+        'NAME': 'db2',
+        'USER': 'loyac',
         'PASSWORD': '221291',
         'HOST': 'localhost',
         'PORT': '',
