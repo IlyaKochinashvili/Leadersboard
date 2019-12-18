57258c7fc7bd2c560e78506c748c23b3202e4e3f
Ilya <kochinashviliilya@gmail.com>
2019-12-18 18:08:42 +0200

    some update

--- leaderboard/settings_drod.py
+++ leaderboard/settings_drod.py
@@ -1,12 +1,11 @@
 DEBUG = False
 ALLOWED_HOSTS = ['*']
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db1',
-        'USER': 'loyacc',
+        'USER': 'loyac',
         'PASSWORD': '221291',
         'HOST': 'localhost',
         'PORT': '',
